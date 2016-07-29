bf44c94aaa3d8b9e7843dacb3ccf4ab5c99e0d3b
Temain <asv.artm@gmail.com>
2016-07-29 12:36:51 +0300

    Проба require.js

---
+++ AdvertisingCompany.Web/App/boot/require.config.js
@@ -0,0 +1,14 @@
+﻿var requireConfig = {
+    baseUrl: '/',
+    paths: {
+        'bootstrap': 'scripts/bootstrap/bootstrap',
+        'jquery': 'scripts/jquery/jquery-1.10.2',
+        'knockout': 'scripts/knockout/knockout-3.4.0.debug',
+        'text': 'scripts/require/text'
+    },
+    shim: {
+        'bootstrap': {
+            deps: ['jquery']
+        }
+    }
+}
---
+++ AdvertisingCompany.Web/App/boot/startup.js
@@ -0,0 +1,22 @@
+﻿require.config(requireConfig);
+
+var app = {};
+define(['jquery', 'knockout', 'bootstrap'], function ($, ko)
+{
+    ko.components.register('greeter',
+    {
+        require: 'app/components/greeter/greeting'
+    });
+
+    function AppViewModel() {
+        var self = this;
+        self.view = ko.observable('');
+
+        self.changeView = function (viewName) {
+            self.view(viewName);
+        };
+    }
+
+    app = new AppViewModel();
+    ko.applyBindings();
+});
